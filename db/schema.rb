1d5df1ea306ed54de654436191ab8152c5ab79b1
Ross Kahn <ross.kahn@outlook.com>
2013-06-27 13:05:29 -0400

    Set dev environment up for laptop (cont.)
    Changed the database.yml to work with my laptop username postgres
    configuration. The schema is pretty much the same as before

--- db/schema.rb
+++ db/schema.rb
@@ -104,8 +104,8 @@ ActiveRecord::Schema.define(:version => 20130520215255) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -114,10 +114,13 @@ ActiveRecord::Schema.define(:version => 20130520215255) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
+    t.boolean  "director",               :default => false
     t.string   "name"
-    t.integer  "role_id"								 :null => false		
+    t.integer  "permission_id"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
